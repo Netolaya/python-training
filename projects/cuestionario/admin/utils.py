ac511df50e963a1ba169f857822f6d8f240fb976
Netolaya <olayex@gmail.com>
2022-08-05 22:53:57 -0500

    actualizando cuestionario

--- projects/cuestionario/admin/utils.py
+++ projects/cuestionario/admin/utils.py
@@ -1,3 +1,5 @@
+import json
+import re
 def opcion_invalida(respuesta, opciones):
     try:
         if int(respuesta) in range(1,len(opciones)+1):
@@ -8,3 +10,10 @@ def opcion_invalida(respuesta, opciones):
     except:
         print(f"{respuesta} no es una opcion valida")
         return True
+
+def existe_cuestionario(nombre_cuestionario):
+    archivo = open("cuestionarios.json", "r")
+    data = json.load(archivo)
+    if nombre_cuestionario in data:
+        return True
+    return False
